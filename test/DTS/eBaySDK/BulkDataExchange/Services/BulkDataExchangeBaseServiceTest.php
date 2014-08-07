60cdb81153d71af2320f2667a3f98a7aa560026b
David T. Sadler <davidtsadler@googlemail.com>
2014-08-07 14:36:55 +0100

    Prepare for 0.1.0 release.

--- test/DTS/eBaySDK/BulkDataExchange/Services/BulkDataExchangeBaseServiceTest.php
+++ test/DTS/eBaySDK/BulkDataExchange/Services/BulkDataExchangeBaseServiceTest.php
@@ -1,23 +1,27 @@
 <?php
-
 use DTS\eBaySDK\BulkDataExchange\Services\BulkDataExchangeBaseService;
+use DTS\eBaySDK\Mocks\HttpClient;
 
 class BulkDataExchangeBaseServiceTest extends \PHPUnit_Framework_TestCase
 {
-    private $obj;
-
     protected function setUp()
     {
-        $this->obj = new BulkDataExchangeBaseService();
+        $this->service1 = new BulkDataExchangeBaseService();
+        $this->service2 = new BulkDataExchangeBaseService(array());
+        $this->service3 = new BulkDataExchangeBaseService(array(), new HttpClient());
     }
 
     public function testCanBeCreated()
     {
-        $this->assertInstanceOf('\DTS\eBaySDK\BulkDataExchange\Services\BulkDataExchangeBaseService', $this->obj);
+        $this->assertInstanceOf('\DTS\eBaySDK\BulkDataExchange\Services\BulkDataExchangeBaseService', $this->service1);
+        $this->assertInstanceOf('\DTS\eBaySDK\BulkDataExchange\Services\BulkDataExchangeBaseService', $this->service2);
+        $this->assertInstanceOf('\DTS\eBaySDK\BulkDataExchange\Services\BulkDataExchangeBaseService', $this->service3);
     }
 
     public function testExtendsBaseService()
     {
-        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->obj);
+        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->service1);
+        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->service2);
+        $this->assertInstanceOf('\DTS\eBaySDK\Services\BaseService', $this->service3);
     }
 }
